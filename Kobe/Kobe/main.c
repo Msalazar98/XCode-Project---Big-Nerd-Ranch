6f31b0bd321970237a060c08e94f078041c75bdd
Msalazar98 <salazarmike98@gmail.com>
2014-12-25 22:54:42 -0800

    Initial Commit

---
+++ Kobe/Kobe/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Kobe
+//
+//  Created by Mike Salazar on 12/25/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("You are now going to start the game: Kobe");
+    Three();
+    }
+ void Three(){
+    int random;
+    if(random == 1){
+        printf("three!");
+    }
+    if(random == 0){
+        printf("Kobe get back to defense!");
+    }
+}
